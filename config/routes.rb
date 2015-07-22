237d869770f95bf230e572de49d67ec27aac5dcd
sStamenkova <simona_stamenkova@hotmail.com>
2015-07-22 20:39:57 +0200

    My Products start

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #devise_for :users, controllers: { sessions: "users/sessions" }
   devise_for :users, controllers: { registrations: "users/registrations" }
+  resources :users, only: [:show]
 
  devise_scope :user do
     get "login", to: "users/sessions#new"
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
   end
 
   root to: "static_pages#index"
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/migrate/20150722124608_create_products.rb
+++ db/migrate/20150722124608_create_products.rb
@@ -7,5 +7,7 @@ class CreateProducts < ActiveRecord::Migration
 
       t.timestamps
     end
+
+    add_index :products, :user_id
   end
 end
